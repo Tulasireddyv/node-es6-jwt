974e266d1b177c135879ba1f877a675a81435074
Tulasireddyv <thulasi250@gmail.com>
2018-11-14 19:28:37 +0530

    Db connect for postgress
    Db connection for postgres

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,14 +2,19 @@ const Sequelize = require('sequelize');
 const config = require('./env');
 
 // Set up the config
+const config = require('./config');
+const Sequelize = require('sequelize');
 const sequelize = new Sequelize(config.mysql.database, config.mysql.username, config.mysql.password, {
-  host: config.mysql.host,
-  port: config.mysql.port,
-  dialect: 'mysql',
-  logging: false, // Disable logging
-  operatorsAliases: false, // Disable aliases,
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 9,
+    min: 0,
+    idle: 10000
+  }
 });
 
 sequelize.authenticate();
 
 module.exports = { sequelize, Sequelize };
+
